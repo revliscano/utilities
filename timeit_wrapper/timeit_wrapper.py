1bd71e887676a3c1418676b4f42160c342584958
Rafael <revliscano@gmail.com>
2020-08-03 16:11:13 -0400

    measure_time retur value now shows a more explicit and readable message

--- timeit_wrapper/timeit_wrapper.py
+++ timeit_wrapper/timeit_wrapper.py
@@ -5,6 +5,8 @@ def measure_time(func, *args, number=100, modules=None, **kwargs):
 	globals_ = {'func': func, 'args': args, 'kwargs': kwargs}
 	if modules:
 		globals_.update({module.__name__: module for module in modules})
-	return timeit(f'func(*args, **kwargs)',
-		      number=number,
+	time = timeit(f'func(*args, **kwargs)',
+                      number=number,
 		      globals=globals_)
+	return f"It took {time} secs to run '{func.__name__}' function {number} times"
+
