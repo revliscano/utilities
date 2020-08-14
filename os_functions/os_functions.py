d3d06c00120d5514c17676a511d7c6119f55de59
Rafael <revliscano@gmail.com>
2020-08-14 14:15:54 -0400

    os_functions added
    get_ancestor_dir() function added

---
+++ os_functions/os_functions.py
@@ -0,0 +1,8 @@
+import os
+
+
+def get_ancestor_dir(directory_path, levels=1):
+    ancestor_dir = os.path.abspath(
+        os.path.join(
+            directory_path, *[os.pardir]*levels))
+    return ancestor_dir
